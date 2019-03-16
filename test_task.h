60d7a03ee53e13205d97c7f02c046abfb80314c4
Anton Golovenko <44031200+Antchel@users.noreply.github.com>
2019-03-16 16:16:53 +0500

    Update test_task.h
    Fix input data logic

--- test_task.h
+++ test_task.h
@@ -13,7 +13,7 @@ void sort_array(int16_t n, int *a);
 int get_array_length ()
 {
     int16_t n;
-    cout << "Enter array size ";
+//    cout << "Enter array size ";
     cin >> n;
     if (n<=0)
     {
@@ -25,7 +25,7 @@ int get_array_length ()
 int *get_array_data(int16_t n)
 {
     int *a;
-    cout << "Enter array elements ";
+//    cout << "Enter array elements ";
     a = new int[n];
     for (int i = 0; i < n; i++)
     {
