a91c103983c244f6a67b56acc3d1ff94e0107997
Antchel <golan349@gmail.com>
2020-01-05 18:02:44 +0500

    [Style] Change coed style

--- main_functions.cpp
+++ main_functions.cpp
@@ -10,7 +10,7 @@ int get_array_length ()
     cin >> n;
     if (n<=0)
     {
-        cout<<"Wrong array length, repeat enter "<<endl;
+        cout<<"Wrong array length, repeat enter "<< endl;
         cin >> n;
     }
     return n;
@@ -28,41 +28,41 @@ int *get_array_data(int16_t n)
 }
 void sort_array(int16_t n, int *a)
 {
-    for(int i=0;i<n;i++)
+    for( int i = 0;i < n;i ++)
     {
-       if(a[i]%2==0)
+       if(a[i] % 2 == 0)
        {
            int pos = i;
            int tmp = a[i];
-           for(int j=i+1;j<n;j++)
+           for(int j = i+1; j < n; j++)
             {
-               if(a[j]<tmp and a[j]%2==0)
+               if(a[j] < tmp and a[j] % 2 == 0)
                     {
                         pos=j;
                         tmp=a[j];
                     }
             }
-           a[pos]=a[i];
-           a[i]=tmp;
+           a[pos] = a[i];
+           a[i] = tmp;
            cout << a[i] << " " ;
        }
     }
-    for(int i=0;i<n;i++)
+    for(int i = 0;i < n; i++)
     {
-       if(a[i]%2!=0)
+       if(a[i] % 2 != 0)
        {
            int pos = i;
            int tmp = a[i];
-           for(int j=i+1;j<n;j++)
+           for(int j = i+1; j < n; j++)
            {
-               if(a[j]>tmp and a[j]%2!=0)
+               if(a[j] > tmp and a[j] % 2 != 0)
                {
-                    pos=j;
-                    tmp=a[j];
+                    pos = j;
+                    tmp = a[j];
                 }
            }
-               a[pos]=a[i];
-               a[i]=tmp;
+               a[pos] = a[i];
+               a[i] = tmp;
                cout << a[i] << " " ;
        }
    }
